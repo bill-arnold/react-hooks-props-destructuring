c1f2c6c20d2c05644b5fb1941555b616bb6077fa
bill <bill.arnold@student.moringaschool.com>
2023-10-27 12:44:26 +0300

    Complete

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,29 @@
 import React from "react";
 import MovieCard from "./MovieCard";
+import SocialMedia from "./SocialMedia";
+
+// function App() {
+//   const title = "Mad Max";
+//   const posterURL =
+//     "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTn1OTYGz2GDC1XjA9tirh_1Rd571yE5UFIYsmZp4nACMd7CCHM";
+//   const genresArr = ["Action", "Adventure", "Science Fiction", "Thriller"];
+
+//   return (
+//     <div className="App">
+//       <MovieCard title={title} genres={genresArr} />
+//     </div>
+//   );
+// }
+
+export default App;
 
 function App() {
-  const title = "Mad Max";
-  const posterURL =
-    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTn1OTYGz2GDC1XjA9tirh_1Rd571yE5UFIYsmZp4nACMd7CCHM";
-  const genresArr = ["Action", "Adventure", "Science Fiction", "Thriller"];
+  const title = "The Matrix";
 
   return (
     <div className="App">
-      <MovieCard title={title} genres={genresArr} />
+      <MovieCard title={title} />
+      <SocialMedia />
     </div>
   );
 }
-
-export default App;
--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,9 +1,10 @@
 import React from "react";
 
+
 function MovieCard({
   title,
+  genres = ["Unknown Genre"],
   posterSrc = "https://m.media-amazon.com/images/M/MV5BOTJjNzczMTUtNzc5MC00ODk0LWEwYjgtNzdiOTEyZmQxNzhmXkEyXkFqcGdeQXVyNzMzMjU5NDY@._V1_UY268_CR1,0,182,268_AL_.jpg",
-  genres,
 }) {
   return (
     <div className="movie-card">
@@ -13,5 +14,6 @@ function MovieCard({
     </div>
   );
 }
+
 
 export default MovieCard;
